cfe576d69cb9b26810a9075485fa67d2c7410528
JPierce28 <joshua.alan.pierce@gmail.com>
2023-04-23 23:18:06 -0600

    Add basic layout for home page with nav buttons

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,36 @@
 import './Home.css'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  const techDirect = () => {
+    navigate('/tech-stack')
+  }
+
+  const projectsDirect = () => {
+    navigate('/projects')
+  }
+
+  const aboutDirect = () => {
+    navigate('/about')
+  }
+
+
   return (
     <section className='home-page'>
       <header className='main-header'>
-        <button className='tech-btn'>Tech Stack</button>
         <h1>Hello there I am Josh</h1>
-        <button className='projects-btn'>Projects</button>
       </header>
+      <div className='tech-stack'>
+        <button onClick={techDirect} className='nav-btn'>Tech Stack</button>
+      </div>
+      <div className='about-container'>
+        <button onClick={aboutDirect} className='nav-btn'>About</button>
+      </div>
+      <div className='projects-container'>
+        <button onClick={projectsDirect} className='nav-btn'>Projects</button>
+      </div>
     </section>
   )
 }
