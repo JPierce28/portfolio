18b3e504e95a787f593bc80821e4ae711fed98a6
JPierce28 <joshua.alan.pierce@gmail.com>
2023-04-24 13:37:04 -0600

    Add layout for projects page with containers for three projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,9 +3,36 @@ import './Projects.css'
 
 const Projects = () => {
   return (
-    <div>
+    <section className='projects-page'>
       <Header/>
-    </div>
+      <h1>My Projects</h1>
+      <section className='projects-container'>
+        <div className='project-card'>
+          <div className='project-image'>
+            <img className='profile-picture' src=''></img>
+          </div>
+          <div className='project-description'>
+            <p>description here</p>
+          </div>
+        </div>
+        <div className='project-card'>
+          <div className='project-image'>
+            <img className='profile-picture' src=''></img>
+          </div>
+          <div className='project-description'>
+            <p>description here</p>
+          </div>
+        </div>
+        <div className='project-card'>
+          <div className='project-image'>
+            <img className='profile-picture' src=''></img>
+          </div>
+          <div className='project-description'>
+            <p>description here</p>
+          </div>
+        </div>
+      </section>
+    </section>
   )
 }
 
