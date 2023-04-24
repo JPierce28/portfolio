d08a3eb459a1f4c55a78aa4d239183e6ea4d6685
JPierce28 <joshua.alan.pierce@gmail.com>
2023-04-24 12:47:33 -0600

    Add new font and container for the about me page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,6 +6,14 @@ const About = () => {
   return (
     <section className='about-page'>
       <Header/>
+      <h1>About Me</h1>
+      <section className='about-description'>
+        <div className='image-container'>
+        </div>
+        <div className='description-container'>
+          <p>I will say things about myself here</p>
+        </div>
+      </section>
     </section>
   )
 }
