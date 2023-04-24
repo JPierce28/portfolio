c92b955c800d0eb1db73c9174e555977306c1109
JPierce28 <joshua.alan.pierce@gmail.com>
2023-04-23 22:51:01 -0600

    Add react router dom and create general paths on the home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,17 @@
 import './Home.css'
+import { Routes, Route } from 'react-router'
+import About from '../About/About'
+import Tech from '../Tech/Tech'
+import Projects from '../Projects/Projects'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+      <Route path='/tech-stack' element={<Tech/>}/>
+    </Routes>
   )
 }
 
