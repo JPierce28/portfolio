4f8aef9ba7af4546444c5614dcbcc44368db0be8
JPierce28 <joshua.alan.pierce@gmail.com>
2023-04-25 13:16:35 -0600

    Add icons for tech stack and change some html and css for styling purposes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,14 +6,16 @@ const About = () => {
   return (
     <section className='about-page'>
       <Header/>
-      <h1>About Me</h1>
-      <section className='about-description'>
-        <div className='image-container'>
-          <img className='profile-picture' src='/images/JPicture.jpg'></img>
-        </div>
-        <div className='description-container'>
-          <p>I will say things about myself here</p>
-        </div>
+      <section className='about-section'>
+        <h1>About Me</h1>
+        <section className='about-description'>
+          <div className='image-container'>
+            <img className='profile-picture' src='/images/JPicture.jpg'></img>
+          </div>
+          <div className='description-container'>
+            <p>I will say things about myself here</p>
+          </div>
+      </section>
       </section>
     </section>
   )
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,32 +5,34 @@ const Projects = () => {
   return (
     <section className='projects-page'>
       <Header/>
+      <section className='my-projects-page'>
       <h1>My Projects</h1>
-      <section className='project-container'>
-        <div className='project-card'>
-          <div className='project-image'>
-            <img className='profile-picture' src='/images/vinyl.png'></img>
+        <section className='project-container'>
+          <div className='project-card'>
+            <div className='project-image'>
+              <img className='profile-picture' src='/images/vinyl.png'></img>
+            </div>
+            <div className='project-description'>
+              <p>description here</p>
+            </div>
           </div>
-          <div className='project-description'>
-            <p>description here</p>
+          <div className='project-card'>
+            <div className='project-image'>
+              <img className='profile-picture' src='/images/hooper.png'></img>
+            </div>
+            <div className='project-description'>
+              <p>description here</p>
+            </div>
           </div>
-        </div>
-        <div className='project-card'>
-          <div className='project-image'>
-            <img className='profile-picture' src='/images/hooper.png'></img>
+          <div className='project-card'>
+            <div className='project-image'>
+              <img className='profile-picture' src='/images/bookworm.png'></img>
+            </div>
+            <div className='project-description'>
+              <p>description here</p>
+            </div>
           </div>
-          <div className='project-description'>
-            <p>description here</p>
-          </div>
-        </div>
-        <div className='project-card'>
-          <div className='project-image'>
-            <img className='profile-picture' src='/images/bookworm.png'></img>
-          </div>
-          <div className='project-description'>
-            <p>description here</p>
-          </div>
-        </div>
+        </section>
       </section>
     </section>
   )
